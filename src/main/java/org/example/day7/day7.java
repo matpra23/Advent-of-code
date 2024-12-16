8e1f85028bc2331945c4c5b234ef2785eb292214
Mateusz Praski <prask1.m3@gmail.com>
2024-12-16 20:41:52 +0100

    d7

---
+++ src/main/java/org/example/day7/day7.java
@@ -0,0 +1,15 @@
+package org.example.day7;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class day7 {
+
+    private static final String filePath = "/Users/mateuszpraski/Documents/java/Advent-of-code-2024/src/main/java/org/example/day7/data07.txt";
+
+    public static void main(String[] args) throws Exception {
+        final List<String> lines = Files.readAllLines(Paths.get(filePath));
+
+    }
+}
